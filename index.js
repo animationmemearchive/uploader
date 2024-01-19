b646a569ff9bf00855d2b94ace975394586cc7b4
artificial <59352535+artificialbutter@users.noreply.github.com>
2024-01-18 16:37:36 -0800

    definitely didn't push an api key

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 import { Client, Storage, Databases, ID, InputFile } from "node-appwrite";
 import fs from "fs";
 import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const client = new Client();
 
 const storage = new Storage(client);
-
 const databases = new Databases(client);
 
 client
     .setEndpoint("https://back.arti.lol/v1")
     .setProject("65a995acebc18055db26")
-    .setKey('432e8835a7f5bc1cd015c58e577b53e6fd22f0e3283a9f5178b182a449a28508fed4b8fd46517005ae2b8b5b1fcc765ecc396f66062a76fc6c02326e71c33de00b1e2907462693c914e0d05dff78ff7cf8c3a25068e57d57b64f0b6fedab186ead5c4fcdcb404ca4b1fd4968064d3d8fd2bb7a97ca1d6c1754183bafdf0a584c')
+    .setKey(process.env.APPWRITE_KEY)
     ;
-
-const filePath = "C:\\Users\\Artificial\\Desktop\\media\\minion vrchat.mp4";
-
 
 async function uploadFile(filePath, creator) {
     // get datecreated from file
